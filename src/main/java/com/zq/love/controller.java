37fc8ad0aade650443a9e8bb92607bc7b1a8e832
周全 <605546609@qq.com>
2023-04-15 11:23:12 +0800

    love

--- src/main/java/com/zq/love/controller.java
+++ src/main/java/com/zq/love/controller.java
@@ -1,7 +1,20 @@
 package com.zq.love;
 
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
 public class controller {
-    public String myLoveAsk(){
-        return "周全爱张亚男";
+    @GetMapping ("/login")
+    public Object myLoveAsk(){
+        String myask=null;
+        Map map=new HashMap<>();
+        map.put("周全:","爱张亚男");
+        myask= "周全:"+map.get("周全:").toString();
+        return myask;
     }
 }
